56dced66b5449881ce5d65f35ffff0c401e5aa7e
Lain MultipleInstances <zlainsama@163.com>
2021-07-02 13:40:07 +0800

    made serveronly

--- src/main/java/lain/mods/neutralizer/MobNeutralizer.java
+++ src/main/java/lain/mods/neutralizer/MobNeutralizer.java
@@ -5,14 +5,20 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("mobneutralizer")
 public class MobNeutralizer {
 
     public MobNeutralizer() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (v, n) -> true));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
