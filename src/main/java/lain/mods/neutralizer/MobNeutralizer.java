b8d46992aac62d1fad8ed712e8b8b91d13cc383e
Lain MultipleInstances <zlainsama@163.com>
2021-07-23 23:32:16 +0800

    update to 1.17.1
    switch to java 16

--- src/main/java/lain/mods/neutralizer/MobNeutralizer.java
+++ src/main/java/lain/mods/neutralizer/MobNeutralizer.java
@@ -1,30 +1,29 @@
 package lain.mods.neutralizer;
 
-import net.minecraft.entity.monster.IMob;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod("mobneutralizer")
 public class MobNeutralizer {
 
     public MobNeutralizer() {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (v, n) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (v, n) -> true));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
     private void onLivingVisibilityCheck(LivingEvent.LivingVisibilityEvent event) {
-        if (event.getLookingEntity() instanceof IMob)
-            if (event.getEntityLiving() instanceof ServerPlayerEntity)
+        if (event.getLookingEntity() instanceof Enemy)
+            if (event.getEntityLiving() instanceof ServerPlayer)
                 if (!(event.getEntityLiving() instanceof FakePlayer))
                     event.modifyVisibility(0.0D);
     }
