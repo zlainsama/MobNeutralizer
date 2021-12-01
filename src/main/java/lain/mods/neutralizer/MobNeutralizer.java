b5ee96879064620fd7ebe6cca2c65d5d5872a24a
Lain MultipleInstances <zlainsama@163.com>
2021-12-01 13:11:17 +0800

    update to 1.18
    switch to java 17

--- src/main/java/lain/mods/neutralizer/MobNeutralizer.java
+++ src/main/java/lain/mods/neutralizer/MobNeutralizer.java
@@ -10,13 +10,13 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod("mobneutralizer")
 public class MobNeutralizer {
 
     public MobNeutralizer() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (v, n) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (v, n) -> true));
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
