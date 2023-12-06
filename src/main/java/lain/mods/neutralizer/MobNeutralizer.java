bf8fccedb8296fe4c9f432e67a82991cd437bd7f
Lain MultipleInstances <zlainsama@live.com>
2023-12-07 07:16:23 +0800

    update to 1.20.3

--- src/main/java/lain/mods/neutralizer/MobNeutralizer.java
+++ src/main/java/lain/mods/neutralizer/MobNeutralizer.java
@@ -3,7 +3,6 @@ package lain.mods.neutralizer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -19,8 +18,7 @@ public class MobNeutralizer {
     private void onLivingVisibilityCheck(LivingEvent.LivingVisibilityEvent event) {
         if (event.getLookingEntity() instanceof Enemy)
             if (event.getEntity() instanceof ServerPlayer)
-                if (!(event.getEntity() instanceof FakePlayer))
-                    event.modifyVisibility(0.0D);
+                event.modifyVisibility(0.0D);
     }
 
     private void setup(FMLCommonSetupEvent event) {
