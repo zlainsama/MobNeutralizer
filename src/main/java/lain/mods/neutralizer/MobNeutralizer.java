2d544f784aae630406a08ff56ecb5814abbd6b3c
Lain MultipleInstances <zlainsama@live.com>
2023-12-09 12:36:36 +0800

    port to 1.20.2 neoforge

--- src/main/java/lain/mods/neutralizer/MobNeutralizer.java
+++ src/main/java/lain/mods/neutralizer/MobNeutralizer.java
@@ -2,11 +2,12 @@ package lain.mods.neutralizer;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.monster.Enemy;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.common.util.FakePlayer;
+import net.neoforged.neoforge.event.entity.living.LivingEvent;
 
 @Mod("mobneutralizer")
 public class MobNeutralizer {
@@ -18,11 +19,12 @@ public class MobNeutralizer {
     private void onLivingVisibilityCheck(LivingEvent.LivingVisibilityEvent event) {
         if (event.getLookingEntity() instanceof Enemy)
             if (event.getEntity() instanceof ServerPlayer)
-                event.modifyVisibility(0.0D);
+                if (!(event.getEntity() instanceof FakePlayer))
+                    event.modifyVisibility(0.0D);
     }
 
     private void setup(FMLCommonSetupEvent event) {
-        MinecraftForge.EVENT_BUS.addListener(this::onLivingVisibilityCheck);
+        NeoForge.EVENT_BUS.addListener(this::onLivingVisibilityCheck);
     }
 
 }
